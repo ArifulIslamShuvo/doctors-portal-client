645ada847098b6080d58694762ad4d029b604086
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-05-12 03:29:58 +0600

    created top banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from '../src/Pages/Home/Home';
+import About from './Pages/About/About';
+import Navbar from './Pages/Shared/Navbar';
+import Login from './Pages/Login/Login';
+
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
