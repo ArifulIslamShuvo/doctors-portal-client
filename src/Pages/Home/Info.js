311d40098b7746bdb1312eefa064db9c4f6b9180
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-05-12 22:03:21 +0600

    aded info cards

---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import clock from '../../assets/icons/clock.svg';
+import marker from '../../assets/icons/marker.svg';
+import phone from '../../assets/icons/phone.svg';
+import InfoCard from './InfoCard';
+
+
+const Info = () => {
+    return (
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 mx-12 mb-6'>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardInfo="Lorem Ipsum is simply dummy" cardTitle="Opening Hours" className img={clock}></InfoCard>
+            <InfoCard bgClass="bg-accent" cardInfo="Brooklyn, NY 10036, United States" cardTitle="Visit our location" className img={marker}></InfoCard>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardInfo="+000 123 456789" cardTitle="Contact us now" className img={phone}></InfoCard>
+        </div>
+    );
+};
+
+export default Info;
---
+++ src/Pages/Home/InfoCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const InfoCard = ({img, cardTitle, cardInfo, bgClass}) => {
+    return (
+        <div>
+            <div class={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+                <figure>
+                    <img className='ml-8' src={img} alt="clock" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title text-white">{cardTitle}</h2>
+                    <p className='text-white'>{cardInfo}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default InfoCard;
